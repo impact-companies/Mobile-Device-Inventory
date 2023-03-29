ee1900d0025887c0adeac26913673917af1a82ef
Christopher Adatan <cadatan@impactcompanies.com>
2023-03-29 17:54:22 +0000

    Add layout.tsx

---
+++ src/pages/layout.tsx
@@ -0,0 +1,27 @@
+import * as React from "react"
+import type { PageProps } from "gatsby"
+
+// MUI imports
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+const theme = createTheme({
+    palette: {
+        mode: 'dark',
+        background: {
+            default: '#042D63',
+            paper: '#c0cce0',
+        },
+    },
+});
+
+const Layout = ({ children }: any): JSX.Element => {
+    return (
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            {children}
+        </ThemeProvider>
+    )
+}
+
+export default Layout

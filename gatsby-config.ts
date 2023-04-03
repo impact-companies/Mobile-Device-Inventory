51ebd86363cd4b7b8dabb62fa86ec1a83845e66f
Christopher Adatan <cadatan@impactcompanies.com>
2023-04-03 17:04:17 +0000

    Configure gatsby-mongdb plugin

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -9,7 +9,12 @@ const config: GatsbyConfig = {
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-source-mongodb`,
+      options: { dbName: 'app_db', collection: `devices` },
+    }
+  ],
 }
 
 export default config
